2aaaf14debe3ece71028d71ee93e7b26f6a0b4d8
Jonas Allen <Hello@JonasAllen.Codes>
2020-07-04 00:23:51 -0700

    Completed "29. useStaticQuery and graphql" | Query: Learned two ways to do GraphQL queries

--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,29 +1,47 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-const ComponentName = () => {
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          author
-          data
-          description
-          title
-          person {
-            age
-            name
-          }
+const getData = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        data
+        description
+        title
+        person {
+          age
+          name
         }
       }
     }
-  `)
+  }
+`
+
+export const Header = () => {
+  // Long Form: Structured Data
+  const data = useStaticQuery(getData)
+
+  // Short Form: Destructured Data
+  const {
+    site: {
+      siteMetadata: {
+        title,
+        person: { name },
+      },
+    },
+  } = useStaticQuery(getData)
+
   return (
     <div>
-      <h2>{data.site.siteMetadata.person.name}</h2>
-      <h2>{data.site.siteMetadata.person.age}</h2>
+      <h2>Long Form: Structured Data</h2>
+      <h3>Title: {data.site.siteMetadata.title}</h3>
+      <h3>Name : {data.site.siteMetadata.person.name}</h3>
+      <h2>Short Form: Destructured Data</h2>
+      <h3>title: {title}</h3>
+      <h3>name: {name}</h3>
     </div>
   )
 }
 
-export default ComponentName
+export default Header
