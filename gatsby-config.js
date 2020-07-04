0c9717898154ee0f900f481672b11e73c77115c2
Jonas Allen <Hello@JonasAllen.Codes>
2020-07-03 19:21:20 -0700

    Completed "23. siteMetadata" | CONFIG: Learned how to use siteMetadata within gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby Tutirial",
+    description: "some random description",
+    author: "@johndoe",
+    data: ["item 1", "item 2"],
+    person: { name: "peter", age: 32 },
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
