fef4d625c10880d801e8958a39513f50e40762ad
Jonas Allen <Hello@JonasAllen.Codes>
2020-07-03 23:40:20 -0700

    Completed "28. Code Exporter - useStaticQuery" | Query: Setup basic example of Code Exporter and useStaticQuery in examples page

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,13 +1,12 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Header from "../examples/Header"
 
 const examples = () => {
   return (
-    <>
-      <Layout>
-        <h1>Hello from examples page</h1>
-      </Layout>
-    </>
+    <div>
+      <h1>Hello from examples page</h1>
+      <Header />
+    </div>
   )
 }
 
