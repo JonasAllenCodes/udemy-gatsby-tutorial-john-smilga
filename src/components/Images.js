81b422e015e96b3171899b956a5e07650a89e359
Jonas Allen <Hello@JonasAllen.Codes>
2020-07-05 21:01:13 -0700

    Completed "44. Render gatsby-image" | Images: Added images to Images page

--- src/components/Images.js
+++ src/components/Images.js
@@ -7,15 +7,15 @@ const getImages = graphql`
   {
     fixed: file(relativePath: { eq: "image3.jpg" }) {
       childImageSharp {
-        fixed(width: 300) {
-          src
+        fixed(width: 200, grayscale: true) {
+          ...GatsbyImageSharpFixed
         }
       }
     }
     fluid: file(relativePath: { eq: "image2.jpg" }) {
       childImageSharp {
         fluid {
-          src
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
@@ -23,16 +23,29 @@ const getImages = graphql`
 `
 
 const Images = () => {
+  const data = useStaticQuery(getImages)
+
+  console.log(data)
+
   return (
     <section className="images">
       <article className="single-image">
         <h3>Basic Image</h3>
+        <img src={img} width="100%" alt="dog looking at fence" />
       </article>
       <article className="single-image">
         <h3>Fixed Image/Blur</h3>
+        <Image
+          fixed={data.fixed.childImageSharp.fixed}
+          alt="skateboard stall"
+        />
       </article>
       <article className="single-image">
         <h3>Fluid Image/SVG</h3>
+        <Image fluid={data.fluid.childImageSharp.fluid} alt="green eyeball" />
+        <div className="small">
+          <Image fluid={data.fluid.childImageSharp.fluid} alt="green eyeball" />
+        </div>
       </article>
     </section>
   )
